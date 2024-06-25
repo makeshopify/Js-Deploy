b1879381ca27535ba820b15f55ca0434b7059752
oceansurat2 <oceansurat2@gmail.com>
2024-06-24 22:02:50 -0700

    Update signature.js

--- oc-signature/signature.js
+++ oc-signature/signature.js
@@ -1,1 +1,8 @@
 console.log("abc");
+var headTag = document.getElementsByTagName('head')[0]
+const linkforCSSfile = document.createElement("link");
+linkforCSSfile.href = 'signature.css'
+linkforCSSfile.type = 'text/css'
+linkforCSSfile.rel = 'stylesheet'
+headTag.appendChild(linkforCSSfile);
+document.body.appendChild(headTag);
