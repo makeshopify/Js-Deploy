619cf7ba7c6af27bfce504ada62a798530776090
oceansurat2 <oceansurat2@gmail.com>
2024-06-24 22:46:09 -0700

    Update signature.js

--- oc-signature/signature.js
+++ oc-signature/signature.js
@@ -1,8 +1,21 @@
-console.log("abc");
-var headTag = document.getElementsByTagName('head')[0]
-const linkforCSSfile = document.createElement("link");
-linkforCSSfile.href = 'https://plugin999.com/demo/sendmailcorephp/js/oc-signature/signature.css'
-linkforCSSfile.type = 'text/css'
-linkforCSSfile.rel = 'stylesheet'
-headTag.appendChild(linkforCSSfile);
-document.body.appendChild(headTag);
+console.log("abcasdsa");
+
+var linka = document.createElement('link');
+
+// set properties of link tag
+linka.href = 'https://plugin999.com/demo/sendmailcorephp/js/oc-signature/signature.css';
+linka.rel = 'stylesheet';
+linka.type = 'text/css';
+
+// Loaded successfully
+linka.onload = function() {
+	console.log('success');
+};
+
+// Loading failed
+linka.onerror = function() {
+	console.log('error');
+};
+
+// append link element to html
+document.body.appendChild(linka);
